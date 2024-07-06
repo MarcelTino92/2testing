70832ca9a909f915c09c33d4bf255a470709651f
MarcelTino92 <162463171+MarcelTino92@users.noreply.github.com>
2024-07-06 22:04:31 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,4 @@ import streamlit as st
 from streamlit_lottie import st_lottie
 #col1, col2 =st.columns([1, 3])
 
-st_lottie("https://lottie.host/8c7af937-e96c-4955-a94d-199d3bea826e/oodWrMRHNo.json")
-st.title("AI Questions - Part 1 is completed")
-st.title("Please close the tab")
+col1,col2=st.columns([1, 2])
