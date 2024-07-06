4860d2724b0037a2a0caaf00730c0702a40eb7c7
MarcelTino92 <162463171+MarcelTino92@users.noreply.github.com>
2024-07-06 21:40:18 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from streamlit_lottie import st_lottie
 col1, col2 =st.columns(2)
 
 with col1:
-    st_lottie("https://assets5.lottiefiles.com/packages/lf20_V9t630.json")
+    st_lottie("https://lottie.host/8c7af937-e96c-4955-a94d-199d3bea826e/oodWrMRHNo.json")
 
 with col2:
     st.write("The file is shown")
