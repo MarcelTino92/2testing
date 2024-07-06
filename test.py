1ea759f2ca7778da021c00bebefa3af383f61f89
MarcelTino92 <162463171+MarcelTino92@users.noreply.github.com>
2024-07-06 21:41:36 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from streamlit_lottie import st_lottie
-col1, col2 =st.columns(2)
+col1, col2,col3 =st.columns(3)
 
 with col1:
     st_lottie("https://lottie.host/8c7af937-e96c-4955-a94d-199d3bea826e/oodWrMRHNo.json")
 
-with col2:
+with col2 and col3:
     st.header("You have completed this section")
