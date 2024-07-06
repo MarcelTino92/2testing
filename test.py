f0ccd641a6a8288ee8ec8cc7687c0666f4055133
MarcelTino92 <162463171+MarcelTino92@users.noreply.github.com>
2024-07-06 22:06:40 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,3 +3,9 @@ from streamlit_lottie import st_lottie
 #col1, col2 =st.columns([1, 3])
 
 col1,col2=st.columns([1, 2])
+
+with col1:
+  st_lottie("https://lottie.host/8c7af937-e96c-4955-a94d-199d3bea826e/oodWrMRHNo.json")
+
+with col2:
+  st.write("Please close the Tab")
