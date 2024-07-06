beb57489e488acd8a945698f12e3308b4901d318
MarcelTino92 <162463171+MarcelTino92@users.noreply.github.com>
2024-07-06 22:35:35 +0530

    Update test.py

--- test.py
+++ test.py
@@ -30,7 +30,7 @@ with col2:
   st.markdown(
         '''
         <div class="centered-title-container">
-            <div class="centered-title"> Answers are recorded for this section. Please close the Tab</div>
+            <div class="centered-title"> Answers are recorded for this section</div>
         </div>
     )
 
