306b8f3d1196019e1df692df2a12df37e93b7b9a
MarcelTino92 <162463171+MarcelTino92@users.noreply.github.com>
2024-07-06 21:50:45 +0530

    Update test.py

--- test.py
+++ test.py
@@ -6,5 +6,12 @@ with col1:
     st_lottie("https://lottie.host/8c7af937-e96c-4955-a94d-199d3bea826e/oodWrMRHNo.json")
 
 with col2:
-    st.empty()
-    st.title("You have completed this section")
+    st.markdown("""<style>
+        .col2-content {
+            text-align: left;  /* Change to 'right' for right-alignment */
+        }
+    </style>""", unsafe_allow_html=True)
+
+    with st.container(class_="col2-content"):
+        st.empty()  # Optional: Add some space before the title
+        st.title("You have completed this section")
