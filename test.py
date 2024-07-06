22f5ab2280f33ed49f17c2af08cc9cd2731831b6
MarcelTino92 <162463171+MarcelTino92@users.noreply.github.com>
2024-07-06 21:41:58 +0530

    Update test.py

--- test.py
+++ test.py
@@ -5,5 +5,5 @@ col1, col2,col3 =st.columns(3)
 with col1:
     st_lottie("https://lottie.host/8c7af937-e96c-4955-a94d-199d3bea826e/oodWrMRHNo.json")
 
-with col2 and col3:
+with col2&col3:
     st.header("You have completed this section")
