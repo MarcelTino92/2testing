5dc3c23da864254f50e9c10a99d1aa2908706997
MarcelTino92 <162463171+MarcelTino92@users.noreply.github.com>
2024-07-06 22:03:12 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,22 +1,7 @@
 import streamlit as st
 from streamlit_lottie import st_lottie
-col1, col2 =st.columns([1, 3])
+#col1, col2 =st.columns([1, 3])
 
-with col1:
-    st_lottie("https://lottie.host/8c7af937-e96c-4955-a94d-199d3bea826e/oodWrMRHNo.json")
-
-with col2:
-    long_title ="AI Question-Part1 is completed"
-    # Apply CSS styling using st.markdown
-    st.markdown(f"""<style>
-    .title-container {{
-        width: 400px;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        white-space: nowrap;  /* Prevent additional whitespace within the title */
-    }}
-    </style>
-    <div class="title-container">
-    <h1>{long_title}</h1>
-    </div>""", unsafe_allow_html=True)
-    st.title("*Please close this Tab*")
+st_lottie("https://lottie.host/8c7af937-e96c-4955-a94d-199d3bea826e/oodWrMRHNo.json")
+st.title("AI Questions - Part 1 is completed")
+st.title("Please close the tab")
