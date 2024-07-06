cdec55ab8a235ceda0fc7f6df1391cd468bc7727
MarcelTino92 <162463171+MarcelTino92@users.noreply.github.com>
2024-07-06 21:45:20 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from streamlit_lottie import st_lottie
-col1, col2 =st.columns([1, 2])
+col1, col2 =st.columns([1, 3])
 
 with col1:
     st_lottie("https://lottie.host/8c7af937-e96c-4955-a94d-199d3bea826e/oodWrMRHNo.json")
