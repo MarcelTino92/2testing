7b87f303fd8630825ec4d2edc3364c35eefa723b
MarcelTino92 <162463171+MarcelTino92@users.noreply.github.com>
2024-07-06 21:39:51 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 import streamlit as st
 from streamlit_lottie import st_lottie
+col1, col2 =st.columns(2)
 
-with st.echo():
+with col1:
     st_lottie("https://assets5.lottiefiles.com/packages/lf20_V9t630.json")
+
+with col2:
+    st.write("The file is shown")
