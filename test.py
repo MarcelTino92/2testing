527c43887b941b481e3682e799835ae31b5fb4a2
MarcelTino92 <162463171+MarcelTino92@users.noreply.github.com>
2024-07-06 21:47:08 +0530

    Update test.py

--- test.py
+++ test.py
@@ -6,4 +6,5 @@ with col1:
     st_lottie("https://lottie.host/8c7af937-e96c-4955-a94d-199d3bea826e/oodWrMRHNo.json")
 
 with col2:
+    st.write("")
     st.header("You have completed this section")
