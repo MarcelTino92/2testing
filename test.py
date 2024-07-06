4cb78e7719059c6cbc9bd9c497169ce354996045
MarcelTino92 <162463171+MarcelTino92@users.noreply.github.com>
2024-07-06 20:55:09 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,18 @@
-<head>
+
+
+import streamlit as st
+import streamlit.components.v1 as components
+
+# Define the URL you want to allow
+
+# Create the HTML for CSP and iframe
+html_code = f"""
+    <head>
   <meta http-equiv="refresh" content="0; URL=https://huggingface.co/spaces/giswqs/Streamlit" />
 </head>
 <body>
 </body>
+"""
+
+# Display the HTML code
+components.html(html_code, height=650)
