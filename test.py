0df91124c03f6cd5ff2fd6e9a6d041668783b819
MarcelTino92 <162463171+MarcelTino92@users.noreply.github.com>
2024-07-06 22:36:35 +0530

    Update test.py

--- test.py
+++ test.py
@@ -18,7 +18,7 @@ with col2:
             height: 30vh;
         }
         .centered-title {
-            font-size: 32px;
+            font-size: 20px;
             font-weight: bold;
             white-space: nowrap;
             margin: 10px 0;
@@ -30,9 +30,11 @@ with col2:
   st.markdown(
         '''
         <div class="centered-title-container">
-            <div class="centered-title"> Answers are recorded for this section</div>
+            <div class="centered-title">Answers are recorded for this section</div>
+            <div class="centered-title">Please close the Tab</div>
         </div>
-        '''
+        ''',
+        unsafe_allow_html=True
     )
 
 
