83b3d57e11f6458398e1bd43df0f3e87c84e74f9
MarcelTino92 <162463171+MarcelTino92@users.noreply.github.com>
2024-07-06 21:54:21 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,8 +3,7 @@ from streamlit_lottie import st_lottie
 col1, col2 =st.columns([2, 3])
 
 with col1:
-    st.markdown("<img src="https://lottie.host/8c7af937-e96c-4955-a94d-199d3bea826e/oodWrMRHNo.json" width='100' style='display: block; margin: 0 auto;'>" , unsafe_allow_html=True)
-    #st_lottie("https://lottie.host/8c7af937-e96c-4955-a94d-199d3bea826e/oodWrMRHNo.json")
+    st_lottie("https://lottie.host/8c7af937-e96c-4955-a94d-199d3bea826e/oodWrMRHNo.json")
 
 with col2:
     title_alignment=
