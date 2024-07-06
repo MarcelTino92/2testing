80bae4c8431abcc1f00d39a54b9eea999b7cf9ab
MarcelTino92 <162463171+MarcelTino92@users.noreply.github.com>
2024-07-06 22:00:07 +0530

    Update test.py

--- test.py
+++ test.py
@@ -6,5 +6,6 @@ with col1:
     st_lottie("https://lottie.host/8c7af937-e96c-4955-a94d-199d3bea826e/oodWrMRHNo.json")
 
 with col2:
-    st.title("*AI Question-Part1 is completed*")
+    long_title ="AI Question-Part1 is completed"
+    st.markdown(f"""<nobr>{long_title}</nobr>""", unsafe_allow_html=True)
     st.title("*Please close this Tab*")
